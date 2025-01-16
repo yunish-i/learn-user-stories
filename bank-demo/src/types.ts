e24bc5fb9084c8f21d133bd934f6a9be4b866cca
Joydeep Mitra <joy@Joydeeps-MacBook-Pro.local>
2025-01-16 11:27:32 -0500

    cleanup

--- bank-demo/src/types.ts
+++
@@ -1,8 +0,0 @@
-export type AccountType = {
-    id: number,
-    balance: number
-}
-
-export interface BankType {
-    createAccount(username: string, age: number, accountNumber: number): AccountType
-}
